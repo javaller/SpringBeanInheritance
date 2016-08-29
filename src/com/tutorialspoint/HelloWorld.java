c3c1a680fa3d4299eacf4aa22e1c2d898ebcc572
Vadim <vop444@gmail.com>
2016-08-29 21:01:02 +0300

    first commit

---
+++ src/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.tutorialspoint;
+
+public class HelloWorld {
+
+	private String message1;
+	private String message2;
+	
+	public void getMessage1() {
+		System.out.println("Message1 is " + message1);
+	}
+	public void setMessage1(String message1) {
+		this.message1 = message1;
+	}
+	public void getMessage2() {
+		System.out.println("Message2 is " + message2);
+	}
+	public void setMessage2(String message2) {
+		this.message2 = message2;
+	}
+}
